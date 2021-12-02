3b50c22ee8b94a99f824b2462f56b6b052c53d45
bbt-t <FEjfewovhwovHVIEvh221fqiv!>
2021-12-02 11:16:42 +0300

    minor change in date selection

--- handlers/todo_handl.py
+++ handlers/todo_handl.py
@@ -6,8 +6,8 @@ from aiogram.dispatcher.filters import Command
 from aiogram.types import Message, CallbackQuery
 from aiogram_calendar import simple_cal_callback, SimpleCalendar
 
-from config import pkl_key
-from loader import dp, logger_guru
+from config import pkl_key, time_now
+from loader import dp, logger_guru, bot
 from middlewares.throttling import rate_limit
 from utils.todo import ToDo
 
@@ -67,13 +67,18 @@ async def bot_todo(message: Message, state: FSMContext):
 @dp.callback_query_handler(simple_cal_callback.filter(), state='todo')
 async def process_simple_calendar(call: CallbackQuery, callback_data, state: FSMContext):
     selected, date = await SimpleCalendar().process_selection(call, callback_data)
-    if date and selected:
-        time_todo = str(date.date())
-        async with state.proxy() as data:
-            data['date']: str = time_todo
 
-        await call.message.answer(f'Что планируешь на <code>{time_todo}</code> число?')
-        await state.set_state('reception_todo')
+    if date and selected:
+        time_todo = date.date()
+        if time_todo < time_now.date():
+            await bot.answer_callback_query(call.id, 'Выбрать можно только на сегодня и позже !', show_alert=True)
+            await call.message.answer('Выбирай с умом :)', reply_markup=await SimpleCalendar().start_calendar())
+        else:
+            async with state.proxy() as data:
+                data['date']: str = str(time_todo)
+
+            await call.message.answer(f'Что планируешь на <code>{time_todo}</code> число?')
+            await state.set_state('reception_todo')
 
 
 @dp.message_handler(state='reception_todo')

5c3815b6580d375805a41aaeeb0e1f3b0da93944
bbt-t <FEjfewovhwovHVIEvh221fqiv!>
2022-01-24 13:58:31 +0300

    mv file

--- loader.py
+++ loader.py
@@ -8,15 +8,16 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from loguru import logger as logger_guru
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine
 from uvloop import EventLoopPolicy as uvloop_Loop
 
 from config import BOT_TOKEN, redis_for_bot, DB_NAME
-from loguru import logger as logger_guru
 
 
 set_event_loop_policy(uvloop_Loop())
+
 
 storage = RedisStorage2(**redis_for_bot)
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)

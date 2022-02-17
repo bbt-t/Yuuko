08b84996122778274ff089f67809d28cb3b15ea1
bbt-t <FEjfewovhwovHVIEvh221fqiv!>
2022-02-17 21:12:32 +0300

    + index

--- utils/database_manage/sql/sql_table.py
+++ utils/database_manage/sql/sql_table.py
@@ -7,13 +7,13 @@ from utils.misc.enums_data import BotSkins
 
 class Users(Base):
     __tablename__ = 'users'
-    telegram_id = Column(BigInteger, primary_key=True)
-    personal_pass = Column(VARCHAR(32))
+    telegram_id = Column(BigInteger, primary_key=True, index=True)
+    personal_pass = Column(VARCHAR(32), index=True)
     birthday = Column(Date, nullable=True)
     email = Column(VARCHAR(128), nullable=True)
     full_username = Column(VARCHAR(128), nullable=True)
-    selected_bot_skin = Column(Enum(BotSkins), nullable=True)
-    selected_bot_lang = Column(VARCHAR(2), nullable=True)
+    selected_bot_skin = Column(Enum(BotSkins), nullable=True, index=True)
+    selected_bot_lang = Column(VARCHAR(2), nullable=True, index=True)
     created_time = Column(DateTime(timezone=True), server_default=func.now())
     updated_time = Column(DateTime(timezone=True), onupdate=func.now())
 
@@ -21,7 +21,7 @@ class Users(Base):
 class OtherInfo(Base):
     __tablename__ = 'other'
     telegram_id = Column(BigInteger, ForeignKey("users.telegram_id"))
-    name_pass = Column(VARCHAR(256), primary_key=True)
+    name_pass = Column(VARCHAR(256), primary_key=True, index=True)
     pass_items = Column(PickleType, nullable=False)
     created_time = Column(DateTime(timezone=True), server_default=func.now())
     updated_time = Column(DateTime(timezone=True), onupdate=func.now())

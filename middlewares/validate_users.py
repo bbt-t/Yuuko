6dbcfb7788162414ec1c21c45f478bbcb1721f01
bbt-t <FEjfewovhwovHVIEvh221fqiv!>
2022-03-04 01:14:47 +0300

    ...

--- middlewares/validate_users.py
+++ middlewares/validate_users.py
@@ -8,6 +8,7 @@ from utils.database_manage.sql.sql_commands import check_invalid_user
 class ValidateUsers(BaseMiddleware):
 
     async def on_pre_process_message(self, message: Message, data: dict):
-        if message.text != '/start' and await check_invalid_user(telegram_id=message.from_user.id):
+        commands: set = {'/todo', '/horoscope', '/hair', '/pass', '/support', '/set_settings'}
+        if message.text in commands and await check_invalid_user(telegram_id=message.from_user.id):
             await message.answer('start the bot again 💬 /start')
             raise CancelHandler()
